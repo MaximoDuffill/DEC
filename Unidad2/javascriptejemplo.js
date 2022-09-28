20632cbd2f5c5af5be8347fb1e96fd47832455a9
MaximoDuffill <114158257+MaximoDuffill@users.noreply.github.com>
2022-09-28 14:41:02 +0200

    Add files via upload

--- Unidad2/javascriptejemplo.js
+++ Unidad2/javascriptejemplo.js
@@ -1,11 +1,12 @@
 
+   
     //Como enseñar en consola
     console.log ("Hola mundo");
 
     //Como crear una alerta
     alert("Hola mundo");
 
-
+    
     //Como nombrar variables
     //Podemos ver que si aun int edad lo ponemos como texto no da error
     //pero si vemos como no hace la suma si no lo con cadena.
@@ -13,11 +14,25 @@
     const nombre="Rosa García";
     incremento=4;
     nueva_edad=edad+incremento;
+
     console.log(nombre+" dentro de "+incremento +"  años tendrá "+nueva_edad+ " años");
 
     //Como hacer un vector
     let miVector=[1,2,3,4,5,6,7,8,9,10];
-    console.log(miVector);
 
+    console.log(miVector);
     console.log(miVector[5]);
     console.log(miVector.length);
+
+
+    //se utiliza el use stict para que tengas que definir una variable, si no da un error
+    //tienes que ponerla arriba del todo del codigo si no puede fallar y no da error
+    //"use strict";
+    
+    pi=3.14;
+    funcionPrueba();
+
+    function funcionPrueba(){
+      
+        piBIS=3.14;
+    }
