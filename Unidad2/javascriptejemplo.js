20d15e46eecdab53b76dabb1ee254d023fc13a4f
MaximoDuffill <114158257+MaximoDuffill@users.noreply.github.com>
2022-09-28 13:51:02 +0200

    Add files via upload

---
+++ Unidad2/javascriptejemplo.js
@@ -0,0 +1,11 @@
+
+    console.log ("Hola mundo");
+
+    alert("Hola mundo");
+
+
+    let edad="23", nueva_edad, incremento;
+    const nombre="Rosa García";
+    incremento=4;
+    nueva_edad=edad+incremento;
+    console.log(nombre+" dentro de "+incremento +"  años tendrá "+nueva_edad+ " años");
